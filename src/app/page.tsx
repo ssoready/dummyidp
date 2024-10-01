b244f56f614b91fc53205f534ec9a0596f53caf3
Ulysse Carion <ulysse.carion@ssoready.com>
2024-10-01 11:13:24 -0700

    fix copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
 
           <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 md:pb-48 md:pt-32">
             <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight text-gray-950 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
-              Close every deal.
+              SAML made easy
             </h1>
             <p className="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
               DummyIDP lets you test SAML and SCIM without setting up a
